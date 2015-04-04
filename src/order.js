a4233d6144d5cb085c7a708c868f61bae7533244
Madeline Cameron <madeline@madelinecameron.net>
2015-04-04 11:19:29 -0500

    Reformatting Order uses variables rather than JSON now Added customer.js Added urls.json

--- src/order.js
+++ src/order.js
@@ -1,46 +1,79 @@
-function Order(firstName, lastName, address, email) {
-    this.rawJSON = {
-        "Order": {
-            "Address": address.rawJSON,
-            "Coupons": [],
-            "CustomerID": "",
-            "Email": email,
-            "Extension": "",
-            "FirstName": firstName,
-            "LastName": lastName,
-            "LanguageCode": "en",
-            "OrderChannel": "OLO",
-            "OrderID": "",
-            "OrderMethod": "Web",
-            "OrderTaker": null,
-            "Payments": [],
-            "Phone": "",
-            "Products": [],
-            "ServiceMethod": "Delivery",
-            "SourceOrganizationURI": "order.dominos.com",
-            "StoreID": "",
-            "Tags": {},
-            "Version": "1.0",
-            "NoCombine": true,
-            "Partners": {},
-            "NewUser": true,
-            "metaData": {}
-        }
-    };
+var urls = require('urls.json');
+
+function Order(Customer, Address, Store) {
+    this.Address = Address;
+    this.Coupons = [];
+    this.CustomerID = Customer.ID;
+    this.Email = Customer.Email;
+    this.Extension = "";
+    this.FirstName = Customer.FirstName;
+    this.LastName = Customer.LastName;
+    this.LanguageCode = "en";
+    this.OrderChannel = "OLO";
+    this.OrderID = "";
+    this.OrderMethod = "Web";
+    this.OrderTaker = "";
+    this.Payments = [];
+    this.Phone = Customer.Phone;
+    this.Products = [];
+    this.ServiceMethod = "Delivery";
+    this.SourceOrganizationURI = "order.dominos.com";
+    this.StoreID = Store.ID;
+    this.Tags = {};
+    this.Version = "1.0";
+    this.NoCombine = true;
+    this.Partners = {};
+    this.NewUser = true;
+    this.metaData = {};
 }
 
-Order.prototype.addItem = function() {
-
+Order.prototype.addItem = function(Item) {  //Add product to Order
+  this.Products.push(Item)
 };
-Order.prototype.removeItem = function() {
 
+Order.prototype.removeItem = function(Item) {  //Remove product from Order
+  var index = this.Products.indexOf(Item);
+  if(index != -1) {
+    this.Products.splice(index, 1);
+  }
 };
-Order.prototype.validateOrder = function() {
 
+Order.prototype.validateOrder = function(callback) {  //Validate Order
+  if( !order || !callback) {
+      if(callback) {
+        callback({
+          message: "Order object is required!"
+        });
+      }
+      return;
+  }
+
+  httpJson.post(urls.order.validate, JSON.stringify(this), callback);
 };
-Order.prototype.priceOrder = function() {
 
+Order.prototype.priceOrder = function(callback) {
+  if( !order || !callback) {
+      if(callback) {
+        callback({
+          error: {
+            message: "Order object is required!"
+          }
+        });
+      }
+      return;
+  }
+
+  httpJson.post(urls.order.price, JSON.stringify(this), callback);
 };
-Order.prototype.placeOrder = function() {
+Order.prototype.placeOrder = function(callback) {
+  if( !order || !callback) {
+      if(callback) {
+        callback({
+          message: "Order object is required!"
+        });
+      }
+      return;
+  }
 
+  httpJson.post(urls.order.place, JSON.stringify(this), callback);
 };
