565fff715c8313d93f9dfb7ceb2cade587b06f43
Brandon Nozaki Miller <brandon@diginow.it>
2015-06-21 13:18:48 -0700

    fixed JSON formatting and working on examples

--- example/api-examples/dominos-store-data.js
+++ example/api-examples/dominos-store-data.js
@@ -1,31 +1,33 @@
 var pizzapi = require('../../dominos-pizza-api');
 
-// //Get stores by postal code, distance is not as accurate this way
-// pizzapi.Util.findNearbyStores(
-//     '63102',
-//     'Delivery',
-//     function(storeData){
-//         console.log(storeData.result.Stores);
-//     }
-// );
-//
-// //Get stores near an exact address, they will be sorted by distance
-// pizzapi.Util.findNearbyStores(
-//     'St. Louis, MO, 63102',
-//     'Delivery',
-//     function(storeData){
-//         console.log(storeData.result.Stores);
-//     }
-// );
-//
-// //Get stores near an exact multi-line address, they will be sorted by distance
-// pizzapi.Util.findNearbyStores(
-//     '700 Clark Ave, St. Louis, MO, 63102',
-//     'Delivery',
-//     function(storeData){
-//         console.log(storeData.result.Stores);
-//     }
-// );
+ //Get stores by postal code, distance is not as accurate this way
+ pizzapi.Util.findNearbyStores(
+     '63102',
+     'Delivery',
+     function(storeData){
+         console.log(storeData.result.Stores);
+     }
+ );
+
+/*
+
+ //Get stores near an exact address, they will be sorted by distance
+ pizzapi.Util.findNearbyStores(
+     'St. Louis, MO, 63102',
+     'Delivery',
+     function(storeData){
+         console.log(storeData.result.Stores);
+     }
+ );
+
+ //Get stores near an exact multi-line address, they will be sorted by distance
+ pizzapi.Util.findNearbyStores(
+     '700 Clark Ave, St. Louis, MO, 63102',
+     'Delivery',
+     function(storeData){
+         console.log(storeData.result.Stores);
+     }
+ );
 
 //Get Store Info for Store #4336
 var Store = new pizzapi.Store(
@@ -48,3 +50,4 @@ Store.getMenu(
         console.log(storeData.result);
     }
 );
+*/
