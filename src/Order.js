c6c68a2dcf10605ab3fdc19344d28130ad790344
Brandon Nozaki Miller <brandon@diginow.it>
2015-09-20 16:00:38 -0700

    added in ability to merge customer with existing order orders will still be duplicated as before, but if both customer and order are specified when creating a new order, the old orders values will be used only when there are no associated customer values. Useful for duplicating the same order for multiple customers. Effectively merging the customer into the order to create a new order.

--- src/Order.js
+++ src/Order.js
@@ -4,75 +4,83 @@ var urls = require('./urls.json');
 var httpJson = require('./http-json');
 
 var Order = function(parameters) {
+  //default order
+  this.Address = '';
+  this.Coupons = [];
+  this.CustomerID = '';
+  this.Email = '';
+  this.Extension = '';
+  this.FirstName = '';
+  this.LastName = '';
+  this.LanguageCode = 'en';
+  this.OrderChannel = 'OLO';
+  this.OrderID = '';
+  this.OrderMethod = 'Web';
+  this.OrderTaker = null;
+  this.Payments = [];
+  this.Phone = '';
+  this.Products = [];
+  this.Market = '';
+  this.Currency = '';
+  this.ServiceMethod = parameters.deliveryMethod || 'Delivery';
+  this.SourceOrganizationURI = 'order.dominos.com';
+  this.StoreID = parameters.storeID||'';
+  this.Tags = {};
+  this.Version = '1.0';
+  this.NoCombine = true;
+  this.Partners = {};
+  this.NewUser = true;
+  this.metaData = {};
+  this.Amounts = {};
+  this.BusinessDate = '';
+  this.EstimatedWaitMinutes = '';
+  this.PriceOrderTime = '';
+  this.AmountsBreakdown;
+
   if(parameters['customer']) {
     var Customer = parameters.customer;
 
     this.Address = Customer.address;
-    this.Coupons = [];
     this.CustomerID = Customer.ID;
     this.Email = Customer.email;
-    this.Extension = '';
     this.FirstName = Customer.firstName;
     this.LastName = Customer.lastName;
-    this.LanguageCode = 'en';
-    this.OrderChannel = 'OLO';
-    this.OrderID = '';
-    this.OrderMethod = 'Web';
-    this.OrderTaker = null;
-    this.Payments = [];
-    this.Phone = '';
-    this.Products = [];
-    this.Market = '';
-    this.Currency = '';
-    this.ServiceMethod = parameters.deliveryMethod ? parameters.deliveryMethod : 'Delivery';
-    this.SourceOrganizationURI = 'order.dominos.com';
-    this.StoreID = parameters.storeID;
-    this.Tags = {};
-    this.Version = '1.0';
-    this.NoCombine = true;
-    this.Partners = {};
-    this.NewUser = true;
-    this.metaData = {};
-    this.Amounts = {};
-    this.BusinessDate = '';
-    this.EstimatedWaitMinutes = '';
-    this.PriceOrderTime = '';
-    this.AmountsBreakdown;
-
     return this;
   }
   if(parameters['Order']) {  //Used to initialize order object from Dominos results (Also handy for initializing from DB)
     var prevOrder = parameters.Order;
+    var Customer = parameters.customer;
 
-    this.Address = prevOrder.Address;
-    this.Coupons = [];
-    this.CustomerID = prevOrder.CustomerID;
-    this.Email = prevOrder.Email;
-    this.Extension = '';
-    this.FirstName = prevOrder.FirstName;
-    this.LastName = prevOrder.LastName;
-    this.LanguageCode = 'en';
-    this.OrderChannel = 'OLO';
+    this.Address = (Customer)? (
+        (Customer.address)? Customer.address : prevOrder.Address
+    ):prevOrder.Address;
+
+    this.CustomerID = (Customer)? (
+        (Customer.address)? Customer.ID : prevOrder.CustomerID
+    ):prevOrder.CustomerID;
+
+    this.Email = (Customer)? (
+        (Customer.address)? Customer.email : prevOrder.Email
+    ):prevOrder.Email;
+
+    this.FirstName = (Customer)? (
+        (Customer.address)? Customer.firstName : prevOrder.FirstName
+    ):prevOrder.FirstName;
+
+    this.LastName = (Customer)? (
+        (Customer.address)? Customer.lastName : prevOrder.LastName
+    ):prevOrder.LastName;
+
     this.OrderID = prevOrder.OrderID;
-    this.OrderMethod = 'Web';
-    this.OrderTaker = null;
-    this.Payments = [];
-    this.Phone = '';
     this.Products = prevOrder.Products;
     this.Market = prevOrder.Market;
     this.Currency = prevOrder.Currency;
-    this.ServiceMethod = 'Delivery';
-    this.SourceOrganizationURI = 'order.dominos.com';
     this.StoreID = prevOrder.StoreID;
-    this.Tags = {};
-    this.Version = '1.0';
-    this.NoCombine = true;
-    this.Partners = {};
-    this.Amounts = prevOrder.Amounts ? prevOrder.Amounts : {};
-    this.BusinessDate = prevOrder.BusinessDate ? prevOrder.BusinessDate : '';
-    this.EstimatedWaitMinutes = prevOrder.EstimatedWaitMinutes ? prevOrder.EstimatedWaitMinutes : '';
-    this.PriceOrderTime = prevOrder.PriceOrderTime ? prevOrder.PriceOrderTime : '';
-    this.AmountsBreakdown = prevOrder.AmountsBreakdown ? prevOrder.AmountsBreakdown : {};
+    this.Amounts = prevOrder.Amounts || {};
+    this.BusinessDate = prevOrder.BusinessDate || '';
+    this.EstimatedWaitMinutes = prevOrder.EstimatedWaitMinutes || '';
+    this.PriceOrderTime = prevOrder.PriceOrderTime || '';
+    this.AmountsBreakdown = prevOrder.AmountsBreakdown || {};
 
     return this;
   }
