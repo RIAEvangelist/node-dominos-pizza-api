137a5b2a17ef66a1372779f8f2e2bf6ebe00480e
Brandon Nozaki Miller <brandon@diginow.it>
2015-09-20 23:24:02 -0700

    seems you can add cheese to a coke keeping autoremove reverting remaining changes

--- src/Item.js
+++ src/Item.js
@@ -5,22 +5,16 @@ var Item = function(parameters) {
     if(!parameters){
         parameters={}
     }
-    this.Code = parameters.code||null;
-    this.AutoRemove=false;
-    //this.Qty = parameters.quantity||1;
-    //adding product ID causes order to fail
-    //this.ID = 1;
 
-    //this.isNew = true;
-    //Can not default options on items which do not support said options.
-    //they get removed from order.
-    //For example I'm trying to order a PM_14SCREEN (pep&mush pizza) which is a
-    //predefined pizza, so it rejects it.
-    //
-    //Also, cant add cheese to a sprite ;)
-    //
-    //this.Options = { 'C': {'1/1': '1'}, 'X': {'1/1': '1'} };
-    //this.Options = {};
+    this.AutoRemove=false;
+
+    this.Code = parameters.code||null;
+    this.Qty = parameters.quantity||1;
+    this.ID = 1;
+
+    this.isNew = true;
+    this.Options = { 'C': {'1/1': '1'}, 'X': {'1/1': '1'} };
+
     if(parameters.options) {
     	var _this = this;
     	parameters.options.forEach(function(value) {
