929ffd93ef7dd5c216450a998e81501d714dd157
Brandon Nozaki Miller <brandon@diginow.it>
2015-09-20 21:38:33 -0700

    payment does not belong in main API rebuilt in order

--- dominos-pizza-api.js
+++ dominos-pizza-api.js
@@ -1,15 +1,3 @@
-function Payment() {  //Probably going to change this so no bother in making its own file.
-    return {
-        "Type": "CreditCard",
-        "Amount": 0,
-        "Number": "",
-        "CardType": "",  //uppercase
-        "Expiration": "",  //digits only
-        "SecurityCode": "",
-        "PostalCode": ""
-    }
-}
-
 module.exports = {
     Order: require('./src/Order'),
     Address: require('./src/Address'),
