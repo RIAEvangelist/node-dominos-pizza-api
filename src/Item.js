2d3c320e47d362664b389576873020f9e0bba397
Brandon Nozaki Miller <brandon@diginow.it>
2015-09-20 22:57:05 -0700

    removed default options from Item, causes items to be rejected if they don't allow the defaults supplied. This is true for items like sprite and brownies as well as the predefined pizza options.

--- src/Item.js
+++ src/Item.js
@@ -9,7 +9,15 @@ var Item = function(parameters) {
     this.Qty = parameters.quantity||1;
     this.ID = 1;
     this.isNew = true;
-    this.Options = { 'C': {'1/1': '1'}, 'X': {'1/1': '1'} };
+    //Can not default options on items which do not support said options.
+    //they get removed from order.
+    //For example I'm trying to order a PM_14SCREEN (pep&mush pizza) which is a
+    //predefined pizza, so it rejects it.
+    //
+    //Also, cant add cheese to a sprite ;)
+    //
+    //this.Options = { 'C': {'1/1': '1'}, 'X': {'1/1': '1'} };
+    this.Options = {}
     if(parameters.options) {
     	var _this = this;
     	parameters.options.forEach(function(value) {
