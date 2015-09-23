7a973ae19f56fa8c5b5dfd9dba0958bfa20b78b8
Julian Hartline <julianh2o@gmail.com>
2015-09-23 07:39:44 +0100

    fixing tabs > spaces

--- src/Item.js
+++ src/Item.js
@@ -16,9 +16,9 @@ var Item = function(parameters) {
     this.Options = { 'C': {'1/1': '1'}, 'X': {'1/1': '1'} };
 
     if(parameters.options) {
-		for (var i=0; i<parameters.options.length; i++) {
-			this.Options[parameters.options[i]] = { '1/1': '1' };
-		}
+        for (var i=0; i<parameters.options.length; i++) {
+            this.Options[parameters.options[i]] = { '1/1': '1' };
+        }
     }
 }
 
