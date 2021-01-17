026ce140c20849accf836bebc31640c56a1361f4
Brandon <brandon@diginow.it>
2021-01-16 17:46:20 -0800

    added type checking to toCase util module

--- utils/toCase.js
+++ utils/toCase.js
@@ -1,8 +1,14 @@
+import Is from 'strong-type';
+
+const is=new Is;
+
 const toCamel=function(key){
+    is.string(key);
     return key[0].toLowerCase()+key.slice(1);
 }
 
 const toPascal=function(key){
+    is.string(key);
     return key[0].toUpperCase()+key.slice(1);
 }
 
