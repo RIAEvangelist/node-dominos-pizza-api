c9a97dd24a6f7a6b230df302923d4199f740b659
Brandon <brandon@diginow.it>
2021-02-25 02:31:10 -0800

    added customer test and beefed up Item test

---
+++ test/customer.js
@@ -0,0 +1,71 @@
+import {Customer} from '../index.js';
+import IsDominos from '../utils/DominosTypes.js';
+
+const isDominos=new IsDominos;
+
+const expected={
+    firstName: 'Barack',
+    lastName: 'Obama',
+    email: 'chief@us.gov',
+    address: {
+      street: '900 Clark Ave',
+      streetNumber: '',
+      streetName: '',
+      unitType: '',
+      unitNumber: '',
+      city: '',
+      region: '',
+      postalCode: '63102',
+      deliveryInstructions: ''
+    },
+    phone: '1-800-555-2368'
+}
+
+const runTest=async function(test){
+    try{
+        test.expects(`Item to populate proper itemCode`);    
+        const customer = new Customer(
+            {
+                address: '900 Clark Ave, 63102',
+                firstName: 'Barack',
+                lastName: 'Obama',
+                phone: '1-800-555-2368',
+                email: 'chief@us.gov'
+            }
+        );
+        
+        isDominos.customer(customer);
+        isDominos.address(customer,address);
+
+        const formattedCustomer=address.formatted;
+
+        for(const [key,value] of Object.entries(expected)){
+            const pascalKey=toPascal(key);
+            
+            //ensure that all customer values match expected values
+            test.compare(
+                value,
+                customer[key],
+                `customer.${key} ${JSON.stringify(customer[key])} to equal expected.${key} ${JSON.stringify(value)}`    
+            );
+
+            //ensure that all dominos formatted values match expected values
+            test.compare(
+                value,
+                formattedCustomer[pascalKey],
+                `expected.${key} ${JSON.stringify(value)} to equal customer.formatted.${pascalKey} ${JSON.stringify(formattedCustomer[pascalKey])}`    
+            );
+        }
+
+    }catch(err){
+        console.trace(err);
+        test.fail();
+    }
+    test.pass();
+    test.done();       
+}
+
+export {
+    runTest as default,
+    runTest
+}
--- test/item.js
+++ test/item.js
@@ -8,16 +8,36 @@ const itemCode='P_14SCREEN';
 const runTest=async function(test){
     try{
         test.expects(`Item to populate proper itemCode`);    
-        const pepperoniPizza=new Item(
+        const item=new Item(
             {
                 code:itemCode
             }
         )
         
-        isDominos.item(pepperoniPizza);
+        isDominos.item(item);
 
-        if(pepperoniPizza.code!=itemCode){
-            throw new ReferenceError(`Expected Item Code of '${pepperoniPizza.code}' to be ${itemCode}.`);
+        if(item.code!=itemCode){
+            throw new ReferenceError(`Expected Item Code of '${item.code}' to be ${itemCode}.`);
+        }
+
+        const formattedItem=address.formatted;
+
+        for(const [key,value] of Object.entries(expected)){
+            const pascalKey=toPascal(key);
+            
+            //ensure that all item values match expected values
+            test.compare(
+                value,
+                item[key],
+                `item.${key} ${JSON.stringify(item[key])} to equal expected.${key} ${JSON.stringify(value)}`    
+            );
+
+            //ensure that all dominos formatted values match expected values
+            test.compare(
+                value,
+                formattedItem[pascalKey],
+                `expected.${key} ${JSON.stringify(value)} to equal item.formatted.${pascalKey} ${JSON.stringify(formattedItem[pascalKey])}`    
+            );
         }
 
     }catch(err){
