fdd92047382841bd2f7d8527d74c8b97e47ba524
Brandon <brandon@diginow.it>
2021-02-24 23:12:39 -0800

    Image test complete

--- test/CI.js
+++ test/CI.js
@@ -4,6 +4,7 @@ import runAddressTest from './address.js';
 import runNearbyStoresTest from './nearbyStore.js';
 import runStoreTest from './store.js';
 import runTrackingTest from './tracking.js';
+import runImageTest from './image.js';
 
 const test=new VanillaTest;
 
@@ -12,6 +13,7 @@ runAddressTest(test);
 await runNearbyStoresTest(test);
 await runStoreTest(test);
 await runTrackingTest(test);
+await runImageTest(test);
 
 
 
--- test/image.js
+++ test/image.js
@@ -1,35 +1,26 @@
-import IsDominos from '../utils/DominosTypes.js';
 import {Menu,Image} from '../index.js';
 
 const menu=await new Menu(4337);
 const productCode=menu.menu.products[
     Object.keys(menu.menu.products)[19]
 ].imageCode;
+const minSize=10e3; //10k
 
-
-const isDominos=new IsDominos;
-
-const testStore=async function(test,storeID,lang){
+const runTest=async function(test){
     try{
         test.expects(`Image to fetch base64 product image for ${productCode}`);    
         const image=await new Image(productCode);
         
-        console.log(image)
+        if(image.base64Image.length<minSize){
+            throw new RangeError(`image size of ${image.base64Image.length} is smaller than the expected minimum size of ${minSize}.`)
+        }
 
     }catch(err){
         console.trace(err);
         test.fail();
     }
     test.pass();
-    test.done();
-}
-
-
-
-
-const runTest=async function(test){
-    
-    
+    test.done();       
 }
 
 export {
